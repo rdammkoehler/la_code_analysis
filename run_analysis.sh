8531267954c28367d455eca864c2144679b5c408
Butch Howard <butch.howard@leadingagile.com>
2021-03-17 13:05:35 -0400

    Shellchecking

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,46 +1,49 @@
 #!/usr/bin/env bash
+#shellcheck disable=SC1091
+#   SC1091 == unable to follow sourced files
+
+
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
   SOURCE="$(readlink "$SOURCE")"
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" 
 done
-export DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-pushd ${DIR} > /dev/null
+
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+export DIR
+pushd "${DIR}" > /dev/null || exit 1
 
 if [[ ! "${#}" -ge 3 ]]; then
     echo "Not enough arguments supplied (${#})"
-    echo 'usage: run_analysis.sh $CODE_ANALYSIS_LOCATION $PRODUCT_CODE_ROOT $ANALYSIS_TARGET'
-    echo "You ran: ${@}"
-    exit -1
+    echo "usage: run_analysis.sh ${CODE_ANALYSIS_LOCATION} ${PRODUCT_CODE_ROOT} ${ANALYSIS_TARGET}"
+    echo "You ran: ${*}"
+    exit 1
 fi
 
 if [[ -z "${1}" ]]; then
     echo "Code Analysis Location is a required parameter"
-    exit -2
+    exit 2
 fi
 
 if [[ -z "${2}" ]]; then
     echo "Product Code Root folder is a required parameter"
-    exit -3
+    exit 3
 fi
 
 if [[ -z "${3}" ]]; then
     echo "Analysis Target folder is a required parameter"
-    exit -4
+    exit 4
 fi
 
 CODE_ANALYSIS_LOCATION=${1}
 PRODUCT_CODE_ROOT=${2}
 code_dir=${3}
 
-# NOTE: the following line assumes we are at the root of the folder structure
-ALL_TOP_LEVEL_FOLDERS=`find ${PRODUCT_CODE_ROOT}/. -type d -depth 2`
-
 # setup/configure code analysis tool(s)
-pushd ${CODE_ANALYSIS_LOCATION} > /dev/null
+pushd "${CODE_ANALYSIS_LOCATION}" > /dev/null || exit 10
 source venv/bin/activate
-popd > /dev/null
+popd > /dev/null || exit 10
 
 # create the analysis command
 # TODO parameterize the following
@@ -50,15 +53,15 @@ ANALYSIS_CMD="python -m metrics.gather --language java -m method_lines balance c
 echo "***** processing: ${code_dir}"
 
 # make sure we're as clean as we can be before starting
-pushd ${code_dir} > /dev/null
-trunk_branch_name=`git branch --show-current`
+pushd "${code_dir}" > /dev/null || exit 10
+trunk_branch_name=$(git branch --show-current)
 git reset --hard
-git checkout ${trunk_branch_name}
+git checkout "${trunk_branch_name}"
 git pull
 rm checkstyle-config.xml
 git reset --hard
-git checkout ${trunk_branch_name}
-popd > /dev/null
+git checkout "${trunk_branch_name}"
+popd > /dev/null || exit 10
 
 # create a viable filename prefix
 repo_name=${code_dir/${PRODUCT_CODE_ROOT}/}
@@ -66,19 +69,19 @@ repo_name=${repo_name/\/\.\//}
 repo_name=${repo_name//\//-}
 
 # run the actual analysis 
-pushd ${CODE_ANALYSIS_LOCATION} > /dev/null
-${ANALYSIS_CMD} -D ${code_dir} --filename ${repo_name} 
-popd > /dev/null
+pushd "${CODE_ANALYSIS_LOCATION}" > /dev/null || exit 10
+${ANALYSIS_CMD} -D "${code_dir}" --filename "${repo_name}" 
+popd > /dev/null || exit 10
 
 # clean up after the run
-pushd ${code_dir} > /dev/null
-git checkout ${trunk_branch_name}
+pushd "${code_dir}" > /dev/null  || exit 10
+git checkout "${trunk_branch_name}"
 git reset --hard
-popd  > /dev/null
+popd  > /dev/null  || exit 10
 
 # move the results to the folder 
-mkdir -p ${code_dir}/../analysis_results
-mv ${CODE_ANALYSIS_LOCATION}/full_results/${repo_name}* ${code_dir}/../analysis_results/.
+mkdir -p "${code_dir}/../analysis_results"
+mv "${CODE_ANALYSIS_LOCATION}/full_results/${repo_name}*" "${code_dir}/../analysis_results/."
 
 
-popd > /dev/null
+popd > /dev/null || exit 10
